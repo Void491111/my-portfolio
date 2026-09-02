2ea169e1b8d2604d3474700d99ae714fc50ff079
unknown <voidw87@gmail.com>
2026-09-02 13:08:55 +0700

    membuat file site.ts untuk pendefinisian

---
+++ src/conifg/site.ts
@@ -0,0 +1,8 @@
+export const site = {
+    name: "Alif Hidayat Salim",
+    role: "Junior Web Developer",
+    location: "Batam, Indonesia",
+    intro: 
+    "Frontend developer focused on TypeScript, React, and Next.js. I design the screen in Figma, then build it.",
+    githubUser: "Void491111"
+} as const;
